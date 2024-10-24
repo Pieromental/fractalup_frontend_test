93e2f2c1197e604dcb4d472383b4886dc8677838
Piero Alvaro Salazar Calle <54618161+Pieromental@users.noreply.github.com>
2024-10-23 19:46:33 -0500

    refactor:Initial commit with GraphQL dependencies

---
+++ apollo.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  client: {
+    service: {
+      name: 'countries-service',
+      url: 'https://countries.trevorblades.com/',
+    },
+    includes: [
+      'src/**/*.vue',
+      'src/**/*.ts',
+      'src/**/*.js',
+      'src/**/*.gql',
+      'src/**/*.graphql',
+    ],
+  },
+};
